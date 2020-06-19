dbf5e279287474bca32166219416576856ec73fa
Fufezan Mihai <fufexan@pm.me>
2020-06-19 13:53:33 +0300

    point 3 implemented

--- inc/js/main.js
+++ inc/js/main.js
@@ -1,89 +1,192 @@
 /*
- * define needed elements
+ * needed elements
+ */
+let money;
+// play type
+const slotType = [numbers, images, emoji];
+// slot elements as array from HtmlCollection
+let slot = [...document.getElementsByClassName('elem')];
+// slot values
+const arr_numbers = ['1', '2', '3'];
+const arr_images = arr_numbers.map( element => '<img src="inc/img/' + element + '.jpg" class="slot-img">' );
+const arr_emoji = ['😍', '🤣', '💩'];
+let playtype;
+// messages
+const messages_fail = ['Oof', 'Too bad', 'Try again', 'Next time', 'Bad luck'];
+const messages_won2 = ['Nice!', 'Good!', 'Well done!', 'Keep going!'];
+const messages_jackpot = ['Astonishing!', 'Amazing!', 'Unbelievable!', 'Miraculous!', 'Incredible!'];
+
+/*
+ * event listeners
  */
 
-// play button
-const play = document.getElementById('play');
-// play type
-const slotType = [...document.getElementsByName('playtype')];
-// slots div
-const slots = document.getElementById('slots');
-// message p
-const message = document.getElementById('message');
-// slot elements as array from HtmlCollection
-const elem = [...document.getElementsByClassName('elem')];
-// images
-let images = ["0", "1", "2"];
-images = images.map( element => '<img src="inc/img/' + element + '.jpg" class="slot-img">' );
-// emoji
-const emoji = ['😍', '🤣', '💩'];
-// numbers
-const numbers = ['1', '2', '3']
+// get play type
+slotType.map( e => e.addEventListener( 'click', getPlayType ) );
+// click play => shuffle
+play.addEventListener( 'click', playGame );
+// reset game
+reset.addEventListener( 'click', resetGame );
+// check when wallet reaches 0
+// TBI
 
 /*
  * game start
  */
-
 getPlayType();
+resetGame();
 
-// set elements to ? before game
+// functions
+
+function doNothing() {}
+
+function resetGame() {
+  resetElements();
+  money = 100;
+  wallet.innerHTML = 'Wallet: ' + money;
+}
+
 function resetElements() {
-	elem.map( element => element.innerHTML = '?' );
-	message.innerHTML = '';
+  slot.map( element => element.innerHTML = '?' );
+  message.innerHTML = '';
+  brokenrule.classList.add('inactive');
+  brokenrule.classList.remove('bad');
+  message.classList.add('inactive');
+  message.classList.remove('bad', 'green', 'gold')
 }
 
 function getPlayType() {
-	let changed = false;
-	for (let i = 0; i < slotType.length && !changed; i++) {
-		if (slotType[i].checked) {
-			changed = i;
-		}
-	}
+  let changed = false;
+  for (let i = 0; i < slotType.length && !changed; i++) {
+    if (slotType[i].checked) {
+      changed = i;
+    }
+  }
 
-	console.log('element changed!', changed);
+  playtype = slotType[changed].id;
+  switch (playtype) {
+  case 'numbers':
+    playtype = arr_numbers;
+    break;
+  case 'images':
+    playtype = arr_images;
+    break;
+  case 'emoji':
+    playtype = arr_emoji;
+    break;
+  }
 
-	playtype = slotType[changed].id;
-	switch (playtype) {
-	case 'numbers':
-		playtype = numbers;
-		break;
-	case 'images':
-		playtype = images;
-		break;
-	case 'emoji':
-		playtype = emoji;
-		break;
-	}
-
-	resetElements();
+  resetElements();
 }
 
 function playGame() {
-	for (let i = 0; i < elem.length; i++ ) {
-		let temp = shuffle(playtype);
-		if (elem[i].innerHTML != temp) {
-			elem[i].innerHTML = temp;
-		}
-	}
+  brokenrule.classList.add('inactive');
+  brokenrule.classList.remove('bad');
+  let result;
+  // get bet
+  // 0 = too much
+  // 1 = not enough
+  // 2 = can play
+  if (money == 0) {
+    result = 1;
+  } else if (bet.value > money) {
+    result = 2;
+  } else if (bet.value < money / 2) {
+    result = 3;
+  } else {
+    result = 0;
+    money -= bet.value;
+  }
 
-	// check if all elements are the same
-	if ( elem.every(e => e.innerHTML === elem[0].innerHTML) ) {
-		result = 'Winner! 🥳';
-	} else {
-		result = 'Oof, loser 😢';
-	}
+  if (!result) {
+    // shuffle slots
+    for (let i = 0; i < slot.length; i++ ) {
+      let temp = shuffle(playtype);
+      if (slot[i].innerHTML != temp) {
+        slot[i].innerHTML = temp;
+      }
+    }
+    let same;
+    // check how many elements are the same
+    if (slot[0].innerHTML == slot[1].innerHTML && slot[0].innerHTML == slot[2].innerHTML) {
+      same = 'jackpot';
+      money += bet.value * 2;
+    } else if (slot[0].innerHTML == slot[1].innerHTML || slot[0].innerHTML == slot[2].innerHTML || slot[1].innerHTML == slot[2].innerHTML) {
+      same = 2;
+      money += bet.value * 1.5;
+    } else {
+      same = 'none';
+    }
+    // set corresponding money and message
+    wallet.innerHTML = 'Wallet: ' + money;
+    switch (same) {
+    case 'none':
+      result = shuffle(messages_fail);
+      message.classList.add('bad')
+      message.classList.remove('green', 'gold');
+      break;
+    case 2:
+      result = shuffle(messages_won2);
+      message.classList.add('green')
+      message.classList.remove('bad', 'gold');
+      break;
+    case 'jackpot':
+      result = shuffle(messages_jackpot)
+      message.classList.add('gold')
+      message.classList.remove('bad', 'green');
+    }
 
-	message.classList.remove('inactive');
-	message.innerHTML = result;
+    message.classList.remove('inactive');
+    message.innerHTML = result;
+  } else if (result == 1) {
+    brokenrule.innerHTML = 'No more money!';
+    brokenrule.classList.remove('inactive');
+    brokenrule.classList.add('bad');
+    resetGame();
+  } else if (result == 2) {
+    brokenrule.innerHTML = 'You bet too much!';
+    brokenrule.classList.remove('inactive');
+    brokenrule.classList.add('bad');
+  } else {
+    brokenrule.innerHTML = 'Bet more!';
+    brokenrule.classList.remove('inactive');
+    brokenrule.classList.add('bad');
+  }
 }
 
-// get play type
-slotType.map( e => e.addEventListener( 'click', getPlayType ) );
-
-// click play => shuffle
-play.addEventListener( 'click',  playGame);
-
 function shuffle(arr) {
-	result = Math.floor(Math.random() * arr.length);
+	let result = Math.floor(Math.random() * arr.length);
 	return arr[result];
+}
+
+// user functions
+function getUsername() {
+  return username.value;
+}
+
+// leaderboard functions
+function checkLB() {
+  if (tbody.innerHTML == '') {
+    leaderboard.classList.add('inactive');
+  }
+}
+
+function resetLB() {
+  LBlength = 0;
+  tbody.innerHTML = '';
+}
+
+function addToLB(content) {
+  let d = '<td>';
+  let e = '</td>'
+  let fcontent = '<tr>' + d + ++LBlength + e + d + content.user + e + d +
+    content.timestamp + e + d + content.score + e + d + content.moves + e + '</tr>';
+  tbody.innerHTML += fcontent;
+}
+
+function updateFirebaseLB() {
+
+}
+
+function GetFirebaseLB() {
+
 }
