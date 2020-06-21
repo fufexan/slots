70065da6c4f382ceec7112a81a67928c59d463c8
Fufezan Mihai <fufexan@pm.me>
2020-06-21 14:14:25 +0300

    part of point 4 implemented

--- inc/js/main.js
+++ inc/js/main.js
@@ -2,6 +2,17 @@
  * needed elements
  */
 let money;
+let LBlength;
+let moves;
+let highscore;
+
+let currentEntry = {
+  user: null,
+  date: null,
+  hs: null,
+  moves: null
+};
+
 // play type
 const slotType = [numbers, images, emoji];
 // slot elements as array from HtmlCollection
@@ -13,7 +24,7 @@ const arr_emoji = ['😍', '🤣', '💩'];
 let playtype;
 // messages
 const messages_fail = ['Oof', 'Too bad', 'Try again', 'Next time', 'Bad luck'];
-const messages_won2 = ['Nice!', 'Good!', 'Well done!', 'Keep going!'];
+const messages_semi = ['Nice!', 'Good!', 'Well done!', 'Keep going!'];
 const messages_jackpot = ['Astonishing!', 'Amazing!', 'Unbelievable!', 'Miraculous!', 'Incredible!'];
 
 /*
@@ -27,24 +38,31 @@ play.addEventListener( 'click', playGame );
 // reset game
 reset.addEventListener( 'click', resetGame );
 // check when wallet reaches 0
-// TBI
+// TBI (if ever)
 
 /*
  * game start
  */
+
+//startGame();
 getPlayType();
 resetGame();
 
 // functions
-
 function doNothing() {}
 
 function resetGame() {
+  addToLB(currentEntry);
+  checkLB();
+  setBet();
   resetElements();
   money = 100;
+  highscore = 0;
+  moves = 0;
   wallet.innerHTML = 'Wallet: ' + money;
 }
 
+// change between numbers, emoji and images
 function resetElements() {
   slot.map( element => element.innerHTML = '?' );
   message.innerHTML = '';
@@ -55,13 +73,16 @@ function resetElements() {
 }
 
 function getPlayType() {
-  let changed = false;
+  let changed = 0;
+  // seems like 0 would not trigger !changed at first glance, but it will be
+  // the changed type if no other element is changed. kind of a neat hack
   for (let i = 0; i < slotType.length && !changed; i++) {
     if (slotType[i].checked) {
       changed = i;
     }
   }
-
+  // because idk how to set arr_(slotType[changed].id)
+  // and don't wanna use eval
   playtype = slotType[changed].id;
   switch (playtype) {
   case 'numbers':
@@ -74,82 +95,81 @@ function getPlayType() {
     playtype = arr_emoji;
     break;
   }
-
   resetElements();
 }
 
 function playGame() {
-  brokenrule.classList.add('inactive');
-  brokenrule.classList.remove('bad');
-  let result;
-  // get bet
-  // 0 = too much
-  // 1 = not enough
-  // 2 = can play
-  if (money == 0) {
-    result = 1;
-  } else if (bet.value > money) {
-    result = 2;
-  } else if (bet.value < money / 2) {
-    result = 3;
-  } else {
-    result = 0;
-    money -= bet.value;
-  }
+  if (username.value != '') {
+    brokenrule.classList.add('inactive');
+    brokenrule.classList.remove('bad');
 
-  if (!result) {
-    // shuffle slots
-    for (let i = 0; i < slot.length; i++ ) {
-      let temp = shuffle(playtype);
-      if (slot[i].innerHTML != temp) {
-        slot[i].innerHTML = temp;
-      }
-    }
-    let same;
-    // check how many elements are the same
-    if (slot[0].innerHTML == slot[1].innerHTML && slot[0].innerHTML == slot[2].innerHTML) {
-      same = 'jackpot';
-      money += bet.value * 2;
-    } else if (slot[0].innerHTML == slot[1].innerHTML || slot[0].innerHTML == slot[2].innerHTML || slot[1].innerHTML == slot[2].innerHTML) {
-      same = 2;
-      money += bet.value * 1.5;
+    let result;
+    // get bet
+    // 0 = can play
+    // 1 = no money
+    // 2 = bet too much
+    // 3 = not enough bet
+    if (money == 0) {
+      result = 1;
+    } else if (bet.value > money) {
+      result = 2;
+    } else if (bet.value < money / 2) {
+      result = 3;
     } else {
-      same = 'none';
-    }
-    // set corresponding money and message
-    wallet.innerHTML = 'Wallet: ' + money;
-    switch (same) {
-    case 'none':
-      result = shuffle(messages_fail);
-      message.classList.add('bad')
-      message.classList.remove('green', 'gold');
-      break;
-    case 2:
-      result = shuffle(messages_won2);
-      message.classList.add('green')
-      message.classList.remove('bad', 'gold');
-      break;
-    case 'jackpot':
-      result = shuffle(messages_jackpot)
-      message.classList.add('gold')
-      message.classList.remove('bad', 'green');
+      result = 0;
+      money -= bet.value;
     }
 
-    message.classList.remove('inactive');
-    message.innerHTML = result;
-  } else if (result == 1) {
-    brokenrule.innerHTML = 'No more money!';
-    brokenrule.classList.remove('inactive');
-    brokenrule.classList.add('bad');
-    resetGame();
-  } else if (result == 2) {
-    brokenrule.innerHTML = 'You bet too much!';
-    brokenrule.classList.remove('inactive');
-    brokenrule.classList.add('bad');
+    if (!result) {
+      // shuffle slots
+      for (let i = 0; i < slot.length; i++ ) {
+        let temp = shuffle(playtype);
+        // save time by not changing same elements
+        if (slot[i].innerHTML != temp) {
+          slot[i].innerHTML = temp;
+        }
+      }
+
+      // check how many elements are the same
+      // 0 - fail, 1 - semi, 2 - jackpot
+      let same;
+      if (slot[0].innerHTML == slot[1].innerHTML && slot[0].innerHTML == slot[2].innerHTML) {
+        same = 2;
+        money += bet.value * 2;
+      } else if (slot[0].innerHTML == slot[1].innerHTML || slot[0].innerHTML == slot[2].innerHTML || slot[1].innerHTML == slot[2].innerHTML) {
+        same = 1;
+        money += bet.value * 1.5;
+      } else {
+        same = 0;
+      }
+
+      // set highscore
+      if (money > highscore) highscore = money;
+        updateLBEntry();
+      // set corresponding money and message
+      wallet.innerHTML = 'Wallet: ' + money;
+      switch (same) {
+      case 0:
+        setMessage(messages_fail, 'bad');
+        break;
+      case 1:
+        setMessage(messages_semi, 'green');
+        break;
+      case 2:
+        setMessage(messages_jackpot, 'gold');
+      }
+    } else if (result == 1) {
+      setBrokenRule('No more money!');
+      resetGame();
+    } else if (result == 2) {
+      setBrokenRule('You bet too much!');
+    } else {
+      setBrokenRule('Bet more!');
+    }
+
+    setBet();
   } else {
-    brokenrule.innerHTML = 'Bet more!';
-    brokenrule.classList.remove('inactive');
-    brokenrule.classList.add('bad');
+    setBrokenRule('Choose a username!');
   }
 }
 
@@ -158,9 +178,42 @@ function shuffle(arr) {
 	return arr[result];
 }
 
-// user functions
+// user
 function getUsername() {
   return username.value;
+}
+
+function newLBEntry() {
+  currentEntry.user = username.value;
+  currentEntry.date = Date().toUTCString();
+  currentEntry.hs = 0;
+  currentEntry.moves = 0;
+}
+
+function updateLBEntry() {
+  currentEntry.moves++;
+  currentEntry.hs = highscore;
+}
+
+// money and message
+function setMessage(arr, color) {
+  let colors = ['bad', 'green', 'gold'];
+  colors.splice(colors.indexOf(color), 1);
+
+  message.innerHTML = shuffle(arr);
+
+  message.classList.add(color);
+  message.classList.remove(colors[0], colors[1], 'inactive');
+}
+
+function setBrokenRule(msg) {
+  brokenrule.innerHTML = msg;
+  brokenrule.classList.add('bad');
+  brokenrule.classList.remove('inactive');
+}
+
+function setBet() {
+  bet.value = Math.ceil(money / 2);
 }
 
 // leaderboard functions
