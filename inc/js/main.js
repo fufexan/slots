a1cd3b360065a7c42fc254f3510163a49c696448
Fufezan Mihai <fufexan@pm.me>
2020-06-22 08:54:36 +0300

    point 4 implemented
    and moved to Vue

--- inc/js/main.js
+++ inc/js/main.js
@@ -1,245 +1,230 @@
-/*
- * needed elements
- */
-let money;
-let LBlength;
-let moves;
-let highscore;
+var vm = new Vue({
+  el: '#vm',
+  data: {
+    money: 100,
+    username: null,
+    bet: null,
+    currentEntry: {
+      user: null,
+      date: null,
+      hs: null,
+      moves: null
+    },
+    slot: ['?', '?', '?'],
 
-let currentEntry = {
-  user: null,
-  date: null,
-  hs: null,
-  moves: null
-};
+    arr_numbers: [1, 2, 3],
+    arr_images: null,
+    arr_emoji: ['😍', '🤣', '💩'],
 
-// play type
-const slotType = [numbers, images, emoji];
-// slot elements as array from HtmlCollection
-let slot = [...document.getElementsByClassName('elem')];
-// slot values
-const arr_numbers = ['1', '2', '3'];
-const arr_images = arr_numbers.map( element => '<img src="inc/img/' + element + '.jpg" class="slot-img">' );
-const arr_emoji = ['😍', '🤣', '💩'];
-let playtype;
-// messages
-const messages_fail = ['Oof', 'Too bad', 'Try again', 'Next time', 'Bad luck'];
-const messages_semi = ['Nice!', 'Good!', 'Well done!', 'Keep going!'];
-const messages_jackpot = ['Astonishing!', 'Amazing!', 'Unbelievable!', 'Miraculous!', 'Incredible!'];
+    messages_fail: ['Oof', 'Too bad', 'Try again', 'Next time', 'Bad luck'],
+    messages_semi: ['Nice!', 'Good!', 'Well done!', 'Keep going!'],
+    messages_jackpot: ['Astonishing!', 'Amazing!', 'Unbelievable!', 'Miraculous!', 'Incredible!'],
 
-/*
- * event listeners
- */
+    playType: null,
+    message: null,
+    brokenrule: null,
+    LBEmpty: true,
+  },
+  methods: {
+    // creates a new entry in the HTML leaderboard with the given content
+    addToLB: function (content) {
+      let d = '<td>';
+      let e = '</td>'
+      let fcontent = '<tr>' + d + e + d + content.user + e + d +
+        content.date + e + d + content.hs + e + d + content.moves + e + '</tr>';
+      tbody.innerHTML += fcontent;
+    },
 
-// get play type
-slotType.map( e => e.addEventListener( 'click', getPlayType ) );
-// click play => shuffle
-play.addEventListener( 'click', playGame );
-// reset game
-reset.addEventListener( 'click', resetGame );
-// check when wallet reaches 0
-// TBI (if ever)
+    // slots get reset to '?' and all messages disappear
+    resetElements: function () {
+      this.slot = this.slot.map( e => e = '?' );
+      this.message = '';
+      this.brokenrule = '';
+    },
 
-/*
- * game start
- */
+    // resets elements and currentEntry after adding to LB
+    // resets money and sets corresponding bet
+    resetGame: function (event) {
+      console.log(event);
+      if (this.currentEntry.user != (null || '') && this.currentEntry.moves) {
+        this.addToLB(this.currentEntry);
+      }
+      this.isLBEmpty();
+      this.newLBEntry();
+      this.resetElements();
+      this.money = 100;
+      this.setBet();
+    },
 
-//startGame();
-getPlayType();
-resetGame();
+    // numbers, images, emoji
+    getPlayType: function (event) {
+      this.playType = vm['arr_' + event.target.id];
+      this.resetElements();
+    },
 
-// functions
-function doNothing() {}
+    // main logic
+    playGame: function () {
+      // check if username is set
+      if (this.username != ('' || null)) {
+        // rules not broken anymore (at least momentarily)
+        this.brokenrule = null;
 
-function resetGame() {
-  addToLB(currentEntry);
-  checkLB();
-  setBet();
-  resetElements();
-  money = 100;
-  highscore = 0;
-  moves = 0;
-  wallet.innerHTML = 'Wallet: ' + money;
-}
-
-// change between numbers, emoji and images
-function resetElements() {
-  slot.map( element => element.innerHTML = '?' );
-  message.innerHTML = '';
-  brokenrule.classList.add('inactive');
-  brokenrule.classList.remove('bad');
-  message.classList.add('inactive');
-  message.classList.remove('bad', 'green', 'gold')
-}
-
-function getPlayType() {
-  let changed = 0;
-  // seems like 0 would not trigger !changed at first glance, but it will be
-  // the changed type if no other element is changed. kind of a neat hack
-  for (let i = 0; i < slotType.length && !changed; i++) {
-    if (slotType[i].checked) {
-      changed = i;
-    }
-  }
-  // because idk how to set arr_(slotType[changed].id)
-  // and don't wanna use eval
-  playtype = slotType[changed].id;
-  switch (playtype) {
-  case 'numbers':
-    playtype = arr_numbers;
-    break;
-  case 'images':
-    playtype = arr_images;
-    break;
-  case 'emoji':
-    playtype = arr_emoji;
-    break;
-  }
-  resetElements();
-}
-
-function playGame() {
-  if (username.value != '') {
-    brokenrule.classList.add('inactive');
-    brokenrule.classList.remove('bad');
-
-    let result;
-    // get bet
-    // 0 = can play
-    // 1 = no money
-    // 2 = bet too much
-    // 3 = not enough bet
-    if (money == 0) {
-      result = 1;
-    } else if (bet.value > money) {
-      result = 2;
-    } else if (bet.value < money / 2) {
-      result = 3;
-    } else {
-      result = 0;
-      money -= bet.value;
-    }
-
-    if (!result) {
-      // shuffle slots
-      for (let i = 0; i < slot.length; i++ ) {
-        let temp = shuffle(playtype);
-        // save time by not changing same elements
-        if (slot[i].innerHTML != temp) {
-          slot[i].innerHTML = temp;
+        let result;
+        // get bet
+        if (this.money == 0) {
+          result = 1;
+          // 1 = no money
+        } else if (this.bet > this.money) {
+          result = 2;
+          // 2 = bet too much
+        } else if (this.bet < this.money / 2) {
+          result = 3;
+          // 3 = not enough bet
+        } else {
+          result = 0;
+          // 0 = can play
+          this.money -= this.bet;
         }
-      }
+        
+        if (!result) {
+          // shuffle slots
+          for (let i = 0; i < this.slot.length; i++ ) {
+            let temp = this.shuffle(this.playType);
+            // save time by not changing same elements
+            if (this.slot[i] != temp) {
+              this.slot[i] = temp;
+            }
+          }
 
-      // check how many elements are the same
-      // 0 - fail, 1 - semi, 2 - jackpot
-      let same;
-      if (slot[0].innerHTML == slot[1].innerHTML && slot[0].innerHTML == slot[2].innerHTML) {
-        same = 2;
-        money += bet.value * 2;
-      } else if (slot[0].innerHTML == slot[1].innerHTML || slot[0].innerHTML == slot[2].innerHTML || slot[1].innerHTML == slot[2].innerHTML) {
-        same = 1;
-        money += bet.value * 1.5;
+          // check how many elements are the same
+          if (this.slot[0] == this.slot[1] && this.slot[0] == this.slot[2]) {
+            this.setMessage(this.messages_jackpot); // jackpot
+            this.money += this.bet * 2;
+          } else if (this.slot[0] == this.slot[1] || this.slot[0] == this.slot[2] || this.slot[1] == this.slot[2]) {
+            this.setMessage(this.messages_semi); // semi win
+            this.money += this.bet * 1.5;
+          } else {
+            this.setMessage(this.messages_fail); // loss
+          }
+
+          // set highscore
+          let what = null;
+          if (this.money > this.currentEntry.hs) {
+            what = 'both';
+          }
+          // auto increments moves no matter what (get it?)
+          this.updateLBEntry(what);
+        } else if (result == 1) {
+          this.brokenrule = 'No more money!';
+          this.resetGame();
+        } else if (result == 2) {
+          this.brokenrule = 'You bet too much!';
+        } else {
+          this.brokenrule = 'Bet more!';
+        }
+        // set new bet to half the money
+        this.setBet();
       } else {
-        same = 0;
+        this.brokenrule = 'Choose a username!';
       }
+    },
 
-      // set highscore
-      if (money > highscore) highscore = money;
-        updateLBEntry();
-      // set corresponding money and message
-      wallet.innerHTML = 'Wallet: ' + money;
-      switch (same) {
-      case 0:
-        setMessage(messages_fail, 'bad');
-        break;
-      case 1:
-        setMessage(messages_semi, 'green');
-        break;
-      case 2:
-        setMessage(messages_jackpot, 'gold');
+    // shuffle any array
+    shuffle: function (arr) {
+      let result = Math.floor(Math.random() * arr.length);
+      return arr[result];
+    },
+
+    // resets currentEntry
+    newLBEntry: function () {
+      this.currentEntry.user = this.username;
+      this.currentEntry.date = this.getDate();
+      this.currentEntry.hs = 0;
+      this.currentEntry.moves = 0;
+    },
+
+    // updates moves [and highscore]
+    updateLBEntry: function (what = null) {
+      this.currentEntry.moves++;
+      if (what == 'both') {
+        this.currentEntry.hs = this.money;
       }
-    } else if (result == 1) {
-      setBrokenRule('No more money!');
-      resetGame();
-    } else if (result == 2) {
-      setBrokenRule('You bet too much!');
-    } else {
-      setBrokenRule('Bet more!');
+    },
+
+    // format date as DD.MM.YYYY HH:MM
+    getDate: function () {
+      let d = new Date();
+      // what ('0' + etc).slice(-2) does: if etc is a digit, it adds 0 before it
+      // if etc is 2 digits, that 0 gets cut at the end
+      let date = ('0' + d.getDate()).slice(-2) + '.' + ('0' + d.getMonth()).slice(-2) + '.' + d.getFullYear();
+      date += ' ' + ('0' + d.getHours()).slice(-2) + ':' + ('0' + d.getMinutes()).slice(-2);
+      return date;
+    },
+
+    // get a random message from arr
+    setMessage: function (arr) {
+      this.message = this.shuffle(arr);
+    },
+
+    // set bet as (a bit more than) half the money
+    setBet: function () {
+      this.bet = Math.ceil(this.money / 2);
+    },
+
+    // leaderboard functions
+    isLBEmpty: function () {
+      if (tbody.innerHTML == '') {
+        this.LBEmpty = true;
+        return true;
+      }
+      else {
+        this.LBEmpty = false;
+        return false;
+      }
+    },
+
+    resetLB: function () {
+      tbody.innerHTML = '';
+      this.isLBEmpty();
+    },
+
+    updateFirebaseLB: function () {
+
+    },
+
+    getFirebaseLB: function () {
+
+    },
+
+    setImages: function (ricardo = false) {
+      this.arr_images = this.arr_numbers.map( e => '<img src="inc/img/' + (ricardo ? 'ricardo' : '') + e + '.jpg" class="slot-img">' );
+      this.playType = this.arr_images;
+    },
+
+    resetHold: function (event) {
+      console.log(event);
+    },
+  },
+  computed: {
+    brokenruleClass: function () {
+      return {
+        inactive: !this.brokenrule,
+        bad: !!this.brokenrule,
+      }
+    },
+    messageClass: function () {
+      return {
+        inactive: !this.message,
+        bad: this.messages_fail.includes(this.message),
+        green: this.messages_semi.includes(this.message),
+        gold: this.messages_jackpot.includes(this.message),
+      }
     }
+  },
+})
 
-    setBet();
-  } else {
-    setBrokenRule('Choose a username!');
-  }
-}
-
-function shuffle(arr) {
-	let result = Math.floor(Math.random() * arr.length);
-	return arr[result];
-}
-
-// user
-function getUsername() {
-  return username.value;
-}
-
-function newLBEntry() {
-  currentEntry.user = username.value;
-  currentEntry.date = Date().toUTCString();
-  currentEntry.hs = 0;
-  currentEntry.moves = 0;
-}
-
-function updateLBEntry() {
-  currentEntry.moves++;
-  currentEntry.hs = highscore;
-}
-
-// money and message
-function setMessage(arr, color) {
-  let colors = ['bad', 'green', 'gold'];
-  colors.splice(colors.indexOf(color), 1);
-
-  message.innerHTML = shuffle(arr);
-
-  message.classList.add(color);
-  message.classList.remove(colors[0], colors[1], 'inactive');
-}
-
-function setBrokenRule(msg) {
-  brokenrule.innerHTML = msg;
-  brokenrule.classList.add('bad');
-  brokenrule.classList.remove('inactive');
-}
-
-function setBet() {
-  bet.value = Math.ceil(money / 2);
-}
-
-// leaderboard functions
-function checkLB() {
-  if (tbody.innerHTML == '') {
-    leaderboard.classList.add('inactive');
-  }
-}
-
-function resetLB() {
-  LBlength = 0;
-  tbody.innerHTML = '';
-}
-
-function addToLB(content) {
-  let d = '<td>';
-  let e = '</td>'
-  let fcontent = '<tr>' + d + ++LBlength + e + d + content.user + e + d +
-    content.timestamp + e + d + content.score + e + d + content.moves + e + '</tr>';
-  tbody.innerHTML += fcontent;
-}
-
-function updateFirebaseLB() {
-
-}
-
-function GetFirebaseLB() {
-
-}
+vm.setImages();
+vm.playType = vm.arr_numbers;
+vm.resetGame();
+tbody.innerHTML = null;
+vm.isLBEmpty();
